86fc8673b07164a5e2525724e0de36008b821d3a
jhashubham7905 <95140370+jhashubham7905@users.noreply.github.com>
2021-12-10 15:12:16 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -317,10 +317,13 @@ def streamlare(url: str) -> str:
         link = re.findall(r'\bhttps?://.*streamlare\.com\S+', url)[0]
     except IndexError:
         raise DirectDownloadLinkException("No streamlare links found\n")
-    url3 = BeautifulSoup(requests.get(link).content, 'html.parser')
-    fin = url3.find("div", {'card card-body text-center'})
-    return fin.find('href')
+    page = BeautifulSoup(requests.get(link).content, 'lxml')
+    info = page.find('div', {'id': 'downloadCollapse'})
+    url3 = info.find('a')
+    return url3.get('href')
 
+
+    
 
 def racaty(url: str) -> str:
     """ Racaty direct link generator
